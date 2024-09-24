9632d3577c34f3345d26b4ddb5a8e3e320fb6629
boki <lsb530@naver.com>
2024-09-24 21:34:35 +0900

    init project

---
+++ spring-db-1/src/test/java/com/boki/jdbc/SpringDb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boki.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
