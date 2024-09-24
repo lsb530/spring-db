69a1042ad5b26b899f27f8a4a44010aa5a023e4a
boki <lsb530@naver.com>
2024-09-25 02:35:59 +0900

    jdbc 연결 & 테스트

---
+++ spring-db-1/src/test/java/com/boki/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,19 @@
+package com.boki.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class DBConnectionUtilTest {
+
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        assertNotNull(connection);
+    }
+
+}
