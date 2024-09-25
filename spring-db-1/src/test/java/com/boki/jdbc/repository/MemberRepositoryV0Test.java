fd6dfef391b6831945e473809bd975551032ed91
boki <lsb530@naver.com>
2024-09-25 10:36:18 +0900

    jdbc 개발 - 조회

--- spring-db-1/src/test/java/com/boki/jdbc/repository/MemberRepositoryV0Test.java
+++ spring-db-1/src/test/java/com/boki/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,18 +1,28 @@
 package com.boki.jdbc.repository;
 
 import com.boki.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws Exception {
-        Member member = new Member("memberV0", 10_000);
+        // save
+        Member member = new Member("memberV4", 10_000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member == findMember {}", member == findMember);
+        log.info("member equals findMember {}", member.equals(findMember));
+        assertEquals(findMember, member);
     }
 
 }
