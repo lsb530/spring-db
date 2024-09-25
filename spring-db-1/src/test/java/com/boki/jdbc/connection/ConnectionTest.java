017bab28ed30857a4420cf7dbd65afa2de44487a
boki <lsb530@naver.com>
2024-09-26 01:32:04 +0900

    DataSource2 - Connection Pool(HikariCP)

--- spring-db-1/src/test/java/com/boki/jdbc/connection/ConnectionTest.java
+++ spring-db-1/src/test/java/com/boki/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.boki.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -29,14 +30,31 @@ public class ConnectionTest {
         // DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 
-        useDataSource(dataSource);
+        useDataSource(dataSource, 2);
     }
 
-    private void useDataSource(DataSource dataSource) throws SQLException {
-        Connection con1 = dataSource.getConnection();
-        Connection con2 = dataSource.getConnection();
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setConnectionTimeout(5_000L); // default: 30초
+        dataSource.setMaximumPoolSize(10); // default: 10
+        dataSource.setPoolName("MyPool");
 
-        log.info("connection={}, class={}", con1, con1.getClass());
-        log.info("connection={}, class={}", con2, con2.getClass());
+        useDataSource(dataSource, 2);
+
+        // 기본 PoolSize를 초과한 요청을 할 시 ConnectionTimeout까지 기다리다 SQLTransientConnectionException 발생
+        // useDataSource(dataSource, 11);
+        Thread.sleep(1000L);
+    }
+
+    private void useDataSource(DataSource dataSource, int connCountReq) throws SQLException {
+        for (int i = 0; i < connCountReq; i++) {
+            Connection conn = dataSource.getConnection();
+            log.info("connection={}, class={}", conn, conn.getClass());
+        }
     }
 }
