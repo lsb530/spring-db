a367f1c28b7415bc96708644f8392767883288b2
boki <lsb530@naver.com>
2024-09-25 11:24:32 +0900

    jdbc 개발 - 수정, 삭제

--- spring-db-1/src/test/java/com/boki/jdbc/repository/MemberRepositoryV0Test.java
+++ spring-db-1/src/test/java/com/boki/jdbc/repository/MemberRepositoryV0Test.java
@@ -4,6 +4,8 @@ import com.boki.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -14,7 +16,7 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws Exception {
         // save
-        Member member = new Member("memberV4", 10_000);
+        Member member = new Member("memberV100", 10_000);
         repository.save(member);
 
         // findById
@@ -23,6 +25,18 @@ class MemberRepositoryV0Test {
         log.info("member == findMember {}", member == findMember);
         log.info("member equals findMember {}", member.equals(findMember));
         assertEquals(findMember, member);
+
+        // update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20_000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        // assertEquals(updatedMember, member); // 실수하지 마세요~
+        assertEquals(20_000, updatedMember.getMoney());
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThrowsExactly(NoSuchElementException.class,
+            () -> repository.findById(member.getMemberId())
+        );
     }
 
 }
