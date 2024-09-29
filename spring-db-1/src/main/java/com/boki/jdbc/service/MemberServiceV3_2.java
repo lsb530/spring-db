eb0d525ce5d27060b9a737c56463cf48073ee57d
boki <lsb530@naver.com>
2024-09-30 08:42:48 +0900

    트랜잭션 문제 해결 - 트랜잭션 템플릿

---
+++ spring-db-1/src/main/java/com/boki/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,51 @@
+package com.boki.jdbc.service;
+
+import com.boki.jdbc.domain.Member;
+import com.boki.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+
+    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 트랜잭션 시작, 성공시 커밋, 실패시 롤백(unchecked exception)
+        txTemplate.executeWithoutResult(status -> {
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) { // 람다에서는 체크 예외를 밖으로 던질 수 없기 때문에 런타임예외로 바꿈
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, fromMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+}
