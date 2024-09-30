d444da858c763e1d17610cd4e8a602c22ce442c9
boki <lsb530@naver.com>
2024-10-01 03:28:41 +0900

    spring-db-2 init project

---
+++ spring-db-2/src/test/java/com/boki/itemservice/SpringDb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boki.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
