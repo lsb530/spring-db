04001848ca6d1c649dbf365c9d03276d1558bad4
boki <lsb530@naver.com>
2024-10-17 00:39:38 +0900

    JPA 적용3 - 예외변환(@Repository AOP)

--- spring-db-2/src/test/java/com/boki/itemservice/domain/ItemRepositoryTest.java
+++ spring-db-2/src/test/java/com/boki/itemservice/domain/ItemRepositoryTest.java
@@ -4,22 +4,18 @@ import com.boki.itemservice.repository.ItemRepository;
 import com.boki.itemservice.repository.ItemSearchCond;
 import com.boki.itemservice.repository.ItemUpdateDto;
 import com.boki.itemservice.repository.memory.MemoryItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 @Transactional
 @SpringBootTest
 class ItemRepositoryTest {
@@ -87,6 +83,8 @@ class ItemRepositoryTest {
         Item item1 = new Item("itemA-1", 10000, 10);
         Item item2 = new Item("itemA-2", 20000, 20);
         Item item3 = new Item("itemB-1", 30000, 30);
+
+        log.info("repository={}", itemRepository.getClass());
 
         itemRepository.save(item1);
         itemRepository.save(item2);
