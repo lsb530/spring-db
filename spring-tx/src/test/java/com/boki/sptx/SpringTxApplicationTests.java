a5396bd950a1086e4ce1b8710ce10861388b32c5
boki <lsb530@naver.com>
2024-10-17 09:18:04 +0900

    spring transaction init project

---
+++ spring-tx/src/test/java/com/boki/sptx/SpringTxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boki.sptx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
