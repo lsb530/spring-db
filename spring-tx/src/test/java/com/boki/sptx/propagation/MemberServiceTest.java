9bf07154847bb8bb4e113ec7c6aa311567f3a0f0
boki <lsb530@naver.com>
2024-10-18 15:28:09 +0900

    트랜잭션 전파 활용7 - 복구 REQUIRES_NEW 서로 다른 conn 사용

--- spring-tx/src/test/java/com/boki/sptx/propagation/MemberServiceTest.java
+++ spring-tx/src/test/java/com/boki/sptx/propagation/MemberServiceTest.java
@@ -122,11 +122,29 @@ class MemberServiceTest {
         //when
         assertThrows(
             UnexpectedRollbackException.class, () ->
-            memberService.joinV2(username)
+                memberService.joinV2(username)
         );
 
         //then: 모든 데이터가 롤백된다
         assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService    @Transactional: ON
+     * memberRepository @Transactional: ON
+     * logRepository    @Transactional: ON(REQUIRES_NEW) Exception
+     */
+    @Test
+    void recoverException_success() {
+        //given
+        String username = "로그예외_recoverException_success";
+
+        //when
+        memberService.joinV2(username);
+
+        //then: member 저장, log 롤백
+        assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isEmpty());
     }
 }
