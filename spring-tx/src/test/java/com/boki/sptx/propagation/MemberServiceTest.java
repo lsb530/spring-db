a47fe41e1633b2bc39213cde827ba4d9ccdb118b
boki <lsb530@naver.com>
2024-10-18 11:08:47 +0900

    트랜잭션 전파 활용2 - 커밋,롤백

--- spring-tx/src/test/java/com/boki/sptx/propagation/MemberServiceTest.java
+++ spring-tx/src/test/java/com/boki/sptx/propagation/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
@@ -33,5 +34,23 @@ class MemberServiceTest {
         //then: 모든 데이터가 정상 저장된다
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService    @Transactional: OFF
+     * memberRepository @Transactional: ON
+     * logRepository    @Transactional: ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        assertThrows(RuntimeException.class, () -> memberService.joinV1(username));
+
+        //then: 모든 데이터가 정상 저장된다
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
     }
 }
